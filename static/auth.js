764a0a7200499df2015ddf33b3973044a48202f3
Prasoon Mishra <173042351+prasoonmishraa@users.noreply.github.com>
2026-04-21 10:52:00 +0530

    Add mouse glow effect on mouse movement

---
+++ static/auth.js
@@ -0,0 +1,6 @@
+const glow = document.getElementById("mouseGlow");
+
+document.addEventListener("mousemove", e => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
